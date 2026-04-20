7448b70a484d2b0b58fc07c258be8c668ec87cb3
bhavekjain <bhavekjain@gmail.com>
2026-04-20 16:34:36 +0530

    finial

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-import events from "@/lib/constants";
+import { IEvent } from "@/database";
+import { cacheLife } from "next/cache";
 
-const page = () => {
+// import events from "@/lib/constants";
+
+const page = async () => {
+  "use cache";
+  cacheLife("hours");
+  const response = await fetch("http://localhost:3000/api/events");
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className="text-center">
@@ -15,11 +23,13 @@ const page = () => {
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {events.map((event: any) => (
-            <li key={event} className="list-none">
-              <EventCard {...event} />
-            </li>
-          ))}
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className="list-none">
+                <EventCard {...event} />
+              </li>
+            ))}
         </ul>
       </div>
     </section>
